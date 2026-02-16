f7be028d3737a1cab468dd08c7ea285bc288194d
anwar108658 <anwar108658@gmail.com>
2026-02-16 12:11:17 +0500

    add welcome page

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonList, IonItem, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonList, IonItem, IonButton, IonRouterLink } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonImg, IonItem, IonButton]
+  imports: [RouterModule,IonContent, CommonModule, FormsModule, IonImg, IonItem, IonButton, IonRouterLink]
 })
 export class WelcomePage implements OnInit {
 
