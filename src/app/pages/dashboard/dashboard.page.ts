4ff6e7ee9c513f865aceb1120eaf9ac7ef14464e
anwar108658 <anwar108658@gmail.com>
2026-03-06 16:13:08 +0500

    add some

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonModal, IonList, IonItem, IonAvatar, IonImg, IonLabel, IonFab, IonFabButton, IonHeader, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonModal, IonList, IonItem, IonAvatar, IonImg, IonLabel, IonFab, IonFabButton, IonHeader, IonToolbar, IonIcon, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonModal, IonList, IonItem, IonAvatar, IonImg, IonLabel, IonFab, IonFabButton, IonHeader, IonToolbar]
+  imports: [IonContent, CommonModule, FormsModule, IonModal, IonList, IonItem, IonAvatar, IonImg, IonLabel, IonHeader, IonIcon, IonButton]
 })
 export class DashboardPage implements OnInit {
 
