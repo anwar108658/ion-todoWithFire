33a1be92d11243078cfc3f64cd9bf195508f11ad
anwar108658 <anwar108658@gmail.com>
2026-03-13 16:02:36 +0500

    add some

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonIcon, IonCard, IonBackButton, IonCardHeader, IonList, IonItem, IonInput, IonButton, IonButtons, IonRouterLink } from '@ionic/angular/standalone';
+import { IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [FormsModule,IonContent, CommonModule, FormsModule]
+  imports: [FormsModule, IonContent, CommonModule, FormsModule, IonIcon, IonButton]
 })
 export class HomePage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  click(e:PointerEvent){
+    console.log()
   }
 
 }
