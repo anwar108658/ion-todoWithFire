8d205a803ee502989b763750c3039be98103108d
anwar108658 <anwar108658@gmail.com>
2026-03-14 16:00:08 +0500

    update home hero section

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonIcon, IonButton, IonProgressBar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [FormsModule, IonContent, CommonModule, FormsModule, IonIcon, IonButton]
+  imports: [FormsModule, IonContent, CommonModule, FormsModule, IonIcon, IonButton, IonProgressBar]
 })
 export class HomePage implements OnInit {
 
