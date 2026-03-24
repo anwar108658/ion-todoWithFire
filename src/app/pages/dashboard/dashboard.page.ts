9582031ce321884baa638c64a7db7330c9111e6f
anwar108658 <anwar108658@gmail.com>
2026-03-24 17:05:17 +0500

    add some

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonModal, IonList, IonItem, IonAvatar, IonImg, IonLabel, IonFab, IonFabButton, IonHeader, IonToolbar, IonIcon, IonButton, IonTabs, IonTabBar, IonTabButton } from '@ionic/angular/standalone';
@@ -9,12 +9,14 @@ import { Router } from '@angular/router';
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonIcon, IonTabs, IonTabBar, IonTabButton, IonFab, IonFabButton]
+  imports: [IonContent, CommonModule, FormsModule, IonIcon, IonTabs, IonTabBar, IonTabButton, IonFab, IonFabButton, IonModal, IonButton]
 })
 export class DashboardPage implements OnInit {
-
-  constructor() {}
-
+  @ViewChild(IonModal) modal!: IonModal;
+  
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
   ngOnInit() {
   }
 
